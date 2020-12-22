80231adb65a76763dcf813ef35247c041a8b790e
maxxant <maxxant@gmail.com>
2020-12-22 08:48:25 +0300

    add moduleinit

---
+++ moduleinit-qt5.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd qt5
+./init-repository -f --module-subset="qtbase,qtcharts,qtimageformats,qtserialport,qtsvg,qtsystems,qttools,qttranslations,qtwebsockets,qtx11extras,qtmultimedia"
