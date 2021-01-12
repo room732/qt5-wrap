15cb8df558fdbe867aa4f8652c56394cae6cd667
maxxant <maxxant@gmail.com>
2021-01-12 17:14:08 +0300

    add module init for qt6

---
+++ moduleinit-qt6.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd qt5
+./init-repository -f --module-subset="qt5compat,qtbase,qtcharts,qtgamepad,qtimageformats,qtmultimedia,qtnetworkauth,qtserialport,qtsvg,qtsystems,qttools,qttranslations,qtwebsockets,qtwinextras,qtx11extras,qtxmlpatterns"
