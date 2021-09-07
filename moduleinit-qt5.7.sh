9c49e2746f30d095d85228f8946d8173193d09c2
maxxant <maxxant@gmail.com>
2021-09-07 16:32:10 +0300

    5.7 init

---
+++ moduleinit-qt5.7.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd qt5
+./init-repository -f --module-subset="qtbase,qtcharts,qtgamepad,qtimageformats,qtmultimedia,qtserialport,qtsvg,qtsystems,qttools,qttranslations,qtwebsockets,qtwinextras,qtx11extras,qtxmlpatterns"
